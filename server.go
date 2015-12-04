67ffded9b2a8c81c1db37a40036f81cb965a2e42
ktorz <matthias.benkort@gmail.com>
2015-12-04 17:55:10 +0100

    Embed font in SVG

--- server.go
+++ server.go
@@ -14,10 +14,6 @@ type svgHandler struct {
 	regSVG regexp.Regexp
 }
 
-type fontHandler struct {
-	fontSrc []byte
-}
-
 func (h *svgHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "image/svg+xml")
 	w.WriteHeader(http.StatusOK)
@@ -32,21 +28,13 @@ func (h *svgHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write(h.regSVG.ReplaceAll(h.svgSrc, city[1]))
 }
 
-func (h *fontHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/x-font-woff")
-	w.WriteHeader(http.StatusOK)
-	w.Write(h.fontSrc)
-}
-
 func init() {
 	svgSrc, err := ioutil.ReadFile("ttn_logo.svg")
-	fontSrc, err := ioutil.ReadFile("leaguespartan-bold.woff")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
 
-	http.Handle("/leaguespartan-bold.woff", &fontHandler{[]byte(fontSrc)})
 	http.Handle("/", &svgHandler{
 		[]byte(svgSrc),
 		*regexp.MustCompile("^/([\\s\\p{L}]+)/?$"),
